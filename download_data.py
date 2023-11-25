76ef2d7d36d2bc57c3c33c5ed4112b7df2298224
Bugaj Bartosz <b.k.bugaj@gmail.com>
2023-11-25 15:52:14 +0100

    Get offers urls per city

--- download_data.py
+++ download_data.py
@@ -1,16 +1,19 @@
 import asyncio
+import pandas as pd
 import re
 import time
 
 from aiohttp import ClientSession
 from bs4 import BeautifulSoup
 from collections import namedtuple
+from unidecode import unidecode
 from typing import Any
 
 
 BASE_URL = "https://www.otodom.pl"
-OFFERS_URL = BASE_URL + "/pl/wyniki/sprzedaz/mieszkanie/opolskie?viewType=listing&limit=72"
-                                         
+OFFERS_BASE_URL = f"{BASE_URL}/pl/wyniki/sprzedaz/mieszkanie"
+ENTRIES_PER_PAGE = 72
+
 HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0'}
 
 DECIMAL_NUMBER_REGEX = r'\d+[,|.]?\d*'
@@ -43,12 +46,31 @@ FIELDS = [
 ]
 
 
+df = pd.read_csv(
+    'populated_places_in_poland.csv', 
+    names=['name', 'type', 'gmina', 'powiat', 'voivodeship', 'registry_id', 'suffix', 'adjective'],
+    sep=',', low_memory=False, header=0)
+df.drop(columns=['registry_id', 'suffix', 'adjective'], inplace=True)
+df = df[df['type'] == 'miasto'].reset_index(drop=True)
+
+
+def _process_row(row):
+    voivodeship = unidecode(row['voivodeship']).lower()
+    powiat = '-'.join(unidecode(row['powiat']).lower().split())
+    gmina = '-'.join(unidecode(row['gmina']).lower().split())
+    city = '-'.join(unidecode(row['name']).lower().split())
+    return OFFERS_BASE_URL + f"/{voivodeship}/{powiat}/{gmina}/{city}" + f"?limit={ENTRIES_PER_PAGE}"
+
+
+cities_offers_urls = df.apply(_process_row, axis=1)
+
+
 async def get_offers_pages_urls(session: ClientSession, url: str) -> list[str]:
     async with session.get(url, headers=HEADERS) as response:
         if response.status == 200:
             parser = BeautifulSoup(await response.text(), 'html.parser')
             pagination = parser.find('nav', attrs={'data-cy':'pagination'})
-            available_pages = pagination.find_all('a', attrs={'class':'eo9qioj1 css-5tvc2l edo3iif1'})
+            available_pages = pagination.find_all('a', attrs={'class':'eo9qioj1 css-5tvc2l edo3iif1'}) if pagination else None
             last_page_number = max([int(page.text) for page in available_pages]) if available_pages else 1
             return [f"{url}&page={number}" for number in range(1, last_page_number + 1)]
 
@@ -92,8 +114,9 @@ async def get_offer_data(session: ClientSession, offer_url: str) -> dict[str, An
 async def get_offers_urls_from_all_pages(url: str) -> list[dict]:
     async with ClientSession() as session:
         pages_urls = await get_offers_pages_urls(session, url)
-        print(len(pages_urls))
-        tasks = [get_offers_urls_from_page(session, url) for url in pages_urls]
+        len_pages_urls = len(pages_urls) if pages_urls else 0
+        print(len_pages_urls)
+        tasks = [get_offers_urls_from_page(session, url) for url in pages_urls] if pages_urls else []
         return await asyncio.gather(*tasks)
 
 
@@ -104,11 +127,13 @@ async def get_offers_data(urls: list[str]) -> list[dict]:
 
 
 async def main():
-    offers_urls = await get_offers_urls_from_all_pages(OFFERS_URL)
-    offers_urls_flat = list(set([url for urls in offers_urls for url in urls]))
-    offers_data = await get_offers_data(offers_urls_flat)
-    print(len(offers_urls_flat))
-    print(len(offers_data))
+    for city_offers_url in cities_offers_urls[:10]:
+        print(city_offers_url)
+        offers_urls = await get_offers_urls_from_all_pages(city_offers_url)
+        offers_urls_flat = list(set([url for urls in offers_urls for url in urls]))
+        offers_data = await get_offers_data(offers_urls_flat)
+        print(len(offers_urls_flat))
+        print(len(offers_data))
 
 
 start_time = time.time()
@@ -116,8 +141,8 @@ asyncio.run(main())
 print(f'ELAPSED TIME: {time.time() - start_time}')
 
 """
-20
-1247
-1247
+PAGES URLS: 20
+OFFERS URLSFLAT: 1247
+OFFERS DATA: 1247
 ELAPSED TIME: 47.78603482246399
 """
